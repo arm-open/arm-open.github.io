54494aadad65b517a82bd62d1a46889dd6ffd260
David Awad <davidawad64@gmail.com>
2016-11-30 21:39:39 -0500

    initial server

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
-from flask import Flask
-from flask import render_template
+import os
+from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def base():
-    return render_template('index')
+    return render_template('index.html')
+
+app.run(
+        host='0.0.0.0',
+        port=int(os.getenv('PORT', 5000))
+        )
