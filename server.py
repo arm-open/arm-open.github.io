18b75c4cc15ff105acb239f792a4ecfe343c581c
David Awad <davidawad64@gmail.com>
2016-12-17 19:12:41 -0500

    site copywriting rough draft

--- server.py
+++ server.py
@@ -1,12 +1,16 @@
 import os
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
 def base():
     return render_template('index.html')
 
-app.run(
-        host='0.0.0.0',
-        port=int(os.getenv('PORT', 5000))
-        )
+
+@app.route('/<path:path>')
+def send_htmls(path):
+    return send_from_directory('templates', path)
+
+
+app.run(host='0.0.0.0',
+        port=int(os.getenv('PORT', 5000)))
