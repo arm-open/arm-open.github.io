784c4232a2a11590d9e8a6d4d3d2e01dc4f3e878
David Awad <davidawad64@gmail.com>
2017-03-30 17:59:45 -0400

    we don't need this anymore

--- static/js/menu.js
+++ static/js/menu.js
@@ -5,23 +5,6 @@ jQuery(document).ready(function($){
 		(!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300) : window.requestAnimationFrame(moveNavigation);
 	});
 
-	/* mobile version - open close navigation
-	$('.cd-nav-trigger').on('click', function(event){
-		event.preventDefault();
-		if($('header').hasClass('nav-is-visible')) $('.moves-out').removeClass('moves-out');
-		
-		$('header').toggleClass('nav-is-visible');
-		$('.cd-main-nav').toggleClass('nav-is-visible');
-		$('.cd-main-content').toggleClass('nav-is-visible');
-	});
-
-	//mobile version - go back to main navigation
-	$('.go-back').on('click', function(event){
-		event.preventDefault();
-		$('.cd-main-nav').removeClass('moves-out');
-	});
-    */
-
 	//open sub-navigation
 	$('.cd-subnav-trigger').on('click', function(event){
 		event.preventDefault();
@@ -31,15 +14,9 @@ jQuery(document).ready(function($){
 	function moveNavigation(){
 		var navigation = $('.cd-main-nav-wrapper');
   		var screenSize = checkWindowWidth();
-        if ( screenSize ) {
-        	//desktop screen - insert navigation inside header element
-			navigation.detach();
-			navigation.insertBefore('.cd-nav-trigger');
-		} else {
-			//mobile screen - insert navigation after .cd-main-content element
-			navigation.detach();
-			navigation.insertAfter('.cd-main-content');
-		}
+        
+        navigation.detach();
+        navigation.insertBefore('.cd-nav-trigger');
 	}
 
 	function checkWindowWidth() {
