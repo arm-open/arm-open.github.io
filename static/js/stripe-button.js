d98c2c9c8ed25e5895b21a0cbc3d4cc3e7039388
arianmoslem <amoslem678@gmail.com>
2017-07-31 13:40:11 -0400

    Fix: JQuery document ready removal
    - Making file access easier for webpack

--- static/js/stripe-button.js
+++ static/js/stripe-button.js
@@ -1,50 +1,46 @@
-$(document).ready(function(){
+$.getScript("https://checkout.stripe.com/checkout.js", function()
+{
+  console.log("stripe checkout script ready");
 
-  $.getScript("https://checkout.stripe.com/checkout.js", function()
-  {
-    console.log("stripe checkout script ready");
+  window.key="SAMPLESTRIPEPUBLISHABLEKEY";
 
-    window.key="SAMPLESTRIPEPUBLISHABLEKEY";
+  $.ajax({ url: '/credentials/publishable_key', async: false, success: function(data) 
+    { 
+      window.key=data;
+    } 
+  });
 
-    $.ajax({ url: '/credentials/publishable_key', async: false, success: function(data) 
-      { 
-        window.key=data;
-      } 
+  var handler = StripeCheckout.configure({
+    key: window.key,
+    image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+    locale: 'auto',
+    token: function(token) {
+  // You can access the token ID with `token.id`.
+  // Get the token ID to your server-side code for use.
+  $("#stripeToken").val(token.id);
+  $("#stripeEmail").val(token.email);
+  $("#stripe-form").submit();
+    }
+  });
+
+  $('#stripe-button').on('click', function (e) {
+  // Open Checkout with further options
+  handler.open({
+    name: 'ARM New Brunswick',
+    description: 'armnewbrunswick.org',
+    amount: 100,
+    panelLabel: 'Donate',
+    zipCode: true,
+    allowRememberMe: false,
+    bitcoin:true
     });
+  e.preventDefault();
+  });
 
-    var handler = StripeCheckout.configure({
-      key: window.key,
-      image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
-      locale: 'auto',
-      token: function(token) {
-    // You can access the token ID with `token.id`.
-    // Get the token ID to your server-side code for use.
-    $("#stripeToken").val(token.id);
-    $("#stripeEmail").val(token.email);
-    $("#stripe-form").submit();
-      }
-    });
-
-    $('#stripe-button').on('click', function (e) {
-    // Open Checkout with further options
-    handler.open({
-      name: 'ARM New Brunswick',
-      description: 'armnewbrunswick.org',
-      amount: 100,
-      panelLabel: 'Donate',
-      zipCode: true,
-      allowRememberMe: false,
-      bitcoin:true
-      });
-    e.preventDefault();
-    });
-
-    // Close Checkout on page navigation
-    $(window).on('popstate', function () {
-      console.log("closed");
-      handler.close();
-    });
-
+  // Close Checkout on page navigation
+  $(window).on('popstate', function () {
+    console.log("closed");
+    handler.close();
   });
 
 });
